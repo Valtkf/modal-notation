28eccd35bb8dca746e29d6092145204d0232150a
Valtkf <valentin.vgregorio@gmail.com>
2024-08-25 16:23:27 +0200

    F1 : Add framer motion and add ButtonNotation component , it's an hover animation with framer motion

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,13 @@
-import { FaRegStar } from "react-icons/fa6";
 import ResetIcon from "../ui/logo/ResetIcon";
+import ButtonNotation from "./ButtonNotation";
 
 export default function Card() {
   return (
-    <div className="text-xl border-[1px] border-[#E0E0E0] rounded-lg bg-white w-[353px] h-[326px]  p-4 transition-shadow duration-400 hover:shadow-lg hover:shadow-gray-400">
-      <div className="flex justify-end items-end mr-2">
-        <ResetIcon />
+    <div className="text-xl border-[1px] border-[#E0E0E0] rounded-lg bg-white w-[353px] h-[326px] p-6 transition-shadow duration-400 hover:shadow-lg hover:shadow-gray-400">
+      <div className="flex justify-end items-end mr-2 mb-6">
+        <button>
+          <ResetIcon />
+        </button>
       </div>
       <div className="flex justify-center items-center flex-col">
         <h1 className="font-semibold text-2xl text-slate-800 mb-4">
@@ -15,23 +17,7 @@ export default function Card() {
           Bon j'espère que tu vas mettre 5 évidemment, si ce n'est pas le cas
           viens me dire pourquoi !
         </p>
-        <div style={{ color: "#7b61ff" }} className="mt-6">
-          <button>
-            <FaRegStar size={48} />
-          </button>
-          <button>
-            <FaRegStar size={48} />
-          </button>
-          <button>
-            <FaRegStar size={48} />
-          </button>
-          <button>
-            <FaRegStar size={48} />
-          </button>
-          <button>
-            <FaRegStar size={48} />
-          </button>
-        </div>
+        <ButtonNotation />
       </div>
     </div>
   );
